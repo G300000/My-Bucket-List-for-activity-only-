442e83f88662c569c2edee544c320ea409df46dd
G300000 <Presheun5@gmail.com>
2025-12-23 23:27:11 +0800

    Add files via upload

---
+++ BucketListApp/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace BucketListApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.labelDream = new System.Windows.Forms.Label();
+            this.txtDream = new System.Windows.Forms.TextBox();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDream
+            // 
+            this.labelDream.AutoSize = true;
+            this.labelDream.Location = new System.Drawing.Point(132, 28);
+            this.labelDream.Name = "labelDream";
+            this.labelDream.Size = new System.Drawing.Size(65, 23);
+            this.labelDream.Text = "Dream:";
+            // 
+            // txtDream
+            // 
+            this.txtDream.BackColor = System.Drawing.Color.Violet;
+            this.txtDream.Location = new System.Drawing.Point(92, 71);
+            this.txtDream.Name = "txtDream";
+            this.txtDream.Size = new System.Drawing.Size(140, 30);
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(115, 145);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(83, 23);
+            this.labelCategory.Text = "Category:";
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.BackColor = System.Drawing.Color.Violet;
+            this.txtCategory.Location = new System.Drawing.Point(92, 182);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(140, 30);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.HotPink;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(38, 266);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(106, 67);
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.DeepPink;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.ForeColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(195, 266);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(106, 67);
+            this.btnLoad.Text = "Load";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(38, 357);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(106, 74);
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(195, 357);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(106, 74);
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvList
+            // 
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.LightPink;
+            this.dgvList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvList.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(335, 28);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.RowHeadersWidth = 51;
+            this.dgvList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvList.Size = new System.Drawing.Size(335, 403);
+            this.dgvList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellClick);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.dgvList);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.labelCategory);
+            this.Controls.Add(this.txtDream);
+            this.Controls.Add(this.labelDream);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Text = "My Fashionable Bucket List";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDream;
+        private System.Windows.Forms.TextBox txtDream;
+        private System.Windows.Forms.Label labelCategory;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvList;
+    }
+}
--- BucketListApp/Form1.cs
+++ BucketListApp/Form1.cs
@@ -1,1 +1,120 @@
+using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
 
+namespace BucketListApp
+{
+    public partial class Form1 : Form
+    {
+        string dbPath = "Data Source=bucketlist.db";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            using (var con = new SQLiteConnection(dbPath))
+            {
+                con.Open();
+                string tableQuery = @"CREATE TABLE IF NOT EXISTS BucketList (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Dream TEXT,
+                                        Category TEXT)";
+                SQLiteCommand cmd = new SQLiteCommand(tableQuery, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtDream.Text == "" || txtCategory.Text == "")
+            {
+                MessageBox.Show("Please enter both Dream and Category!");
+                return;
+            }
+
+            using (var con = new SQLiteConnection(dbPath))
+            {
+                con.Open();
+                string insertQuery = "INSERT INTO BucketList (Dream, Category) VALUES (@dream, @category)";
+                SQLiteCommand cmd = new SQLiteCommand(insertQuery, con);
+                cmd.Parameters.AddWithValue("@dream", txtDream.Text);
+                cmd.Parameters.AddWithValue("@category", txtCategory.Text);
+                cmd.ExecuteNonQuery();
+            }
+            MessageBox.Show("Dream added successfully!");
+            txtDream.Clear();
+            txtCategory.Clear();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (var con = new SQLiteConnection(dbPath))
+            {
+                con.Open();
+                SQLiteDataAdapter da = new SQLiteDataAdapter("SELECT * FROM BucketList", con);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                da.Fill(dt);
+                dgvList.DataSource = dt;
+            }
+        }
+
+        private void dgvList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvList.SelectedRows.Count > 0)
+            {
+                txtDream.Text = dgvList.SelectedRows[0].Cells["Dream"].Value.ToString();
+                txtCategory.Text = dgvList.SelectedRows[0].Cells["Category"].Value.ToString();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvList.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvList.SelectedRows[0].Cells["Id"].Value);
+                using (var con = new SQLiteConnection(dbPath))
+                {
+                    con.Open();
+                    string updateQuery = "UPDATE BucketList SET Dream=@dream, Category=@category WHERE Id=@id";
+                    SQLiteCommand cmd = new SQLiteCommand(updateQuery, con);
+                    cmd.Parameters.AddWithValue("@dream", txtDream.Text);
+                    cmd.Parameters.AddWithValue("@category", txtCategory.Text);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Dream updated successfully!");
+                txtDream.Clear();
+                txtCategory.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvList.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvList.SelectedRows[0].Cells["Id"].Value);
+                using (var con = new SQLiteConnection(dbPath))
+                {
+                    con.Open();
+                    string deleteQuery = "DELETE FROM BucketList WHERE Id=@id";
+                    SQLiteCommand cmd = new SQLiteCommand(deleteQuery, con);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Dream deleted successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete!");
+            }
+        }
+    }
+}
